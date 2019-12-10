0cb8fc66c955c1da0d6219fc4d6e6e67e440e4fd
as1ndu <git@drileba.capital>
2019-12-10 03:28:32 +0300

    upload scilla files to server

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 import base64
 from flask import Flask, request, jsonify
 
+
 app = Flask(__name__)
 
 # Command line interface
@@ -17,8 +18,8 @@ def scilla_file(filename, source):
     with open(path, 'w') as f:
         f.write(source)
 
-    print("*{0} saved to disk.".format(filename))
-      
+    print("* {0} saved to disk.".format(filename))
+       
     return ""
 
 # Warnings & Error messages
@@ -38,12 +39,18 @@ def cashflow_ananysis(filename):
 def debug():
     # Only allow post requests
     if request.method == 'POST':
+
+        #read POST request data
         filename = str(request.values.to_dict(flat=False)['filename'][0])
         source = str(request.values.to_dict(flat=False)['source'][0])
 
-        decoded_source = base64.b64decode(source) # decode scilla source
+        # decode scilla source
+        decoded_source = str(base64.b64decode(source)).encode().decode("unicode-escape")
+        decoded_source = decoded_source.replace("b\'","",1)
+        valid_source =  decoded_source[:len(decoded_source)-1]
 
-        scilla_file(filename, str(decoded_source) ) # Save scilla file to disk
+        # Save scilla file to disk
+        scilla_file(filename,   valid_source)
 
         print("debugged {0} ".format(filename))
 
